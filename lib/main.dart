e63a55b560788fa9e8a9e31741597481f15d32ce
Gospodi <gospodijazzet@gmail.com>
2022-04-29 16:46:05 +0300

    добавили простой текстовый виджет в body

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Bullseye',
-    home: Scaffold(appBar: AppBar(title: const Text('Bullseye'))),
-  ));
+  runApp(
+    MaterialApp(
+      title: 'Bullseye',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Bullseye'),
+        ),
+        body: const Center(
+          child: Text('Hello There!'),
+        ),
+      ),
+    ),
+  );
 }
