a5fd26997cf250a0929720d7e697b5cbc003cc9c
Gospodi <gospodijazzet@gmail.com>
2022-05-04 20:41:05 +0300

    вводим начальное случайное значение

--- lib/prompt.dart
+++ lib/prompt.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class Prompt extends StatelessWidget {
   const Prompt({Key? key}) : super(key: key);
 
+  final int targetValue;
+
   @override
   Widget build(BuildContext context) {
     return Container();
