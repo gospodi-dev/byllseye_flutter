7be0b8bd9499809c790921b3e0473b9c4ee16ea2
Gospodi <gospodijazzet@gmail.com>
2022-05-04 20:44:43 +0300

    добавляем виджет Column для упорядочивания

--- lib/prompt.dart
+++ lib/prompt.dart
@@ -7,6 +7,8 @@ class Prompt extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[],
+    );
   }
 }
