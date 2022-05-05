7d7353f69f526265c709ac50b38e21ad10dedf6f
Gospodi <gospodijazzet@gmail.com>
2022-05-05 18:00:59 +0300

    добавленые два final значения для рекорда и раунда

--- lib/score.dart
+++ lib/score.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Score extends StatelessWidget {
-  const Score({Key? key}) : super(key: key);
+  const Score({Key? key, required this.totalScore, required this.round})
+      : super(key: key);
+
+  final int totalScore;
+  final int round;
 
   @override
   Widget build(BuildContext context) {
