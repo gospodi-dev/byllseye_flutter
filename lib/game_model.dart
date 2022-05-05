7ff6fe9b2839e9d53f760d5777ea96907c36c409
Gospodi <gospodijazzet@gmail.com>
2022-05-05 18:25:07 +0300

    добавлены числовые переменные

--- lib/game_model.dart
+++ lib/game_model.dart
@@ -2,4 +2,9 @@ class GameModel {
   static const sliderStart = 50;
   static const scoreStart = 0;
   static const roundStart = 1;
+
+  int target;
+  int current;
+  int totalScore;
+  int round;
 }
