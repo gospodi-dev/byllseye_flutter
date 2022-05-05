322093413df3bcd846f0361a7132261ef5ad0a15
Gospodi <gospodijazzet@gmail.com>
2022-05-05 16:46:24 +0300

    создан виджет с отслеживанием состояния

--- lib/control.dart
+++ lib/control.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class Control extends StatefulWidget {
+  const Control({Key? key}) : super(key: key);
+
+  @override
+  State<Control> createState() => _ControlState();
+}
+
+class _ControlState extends State<Control> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
