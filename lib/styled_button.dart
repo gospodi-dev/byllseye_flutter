f0029daa244222c1fe7a9d2591b4dc36745e01fe
Gospodi <gospodijazzet@gmail.com>
2022-05-12 16:55:41 +0300

    добавлен возврат кнопки со свойствами

--- lib/styled_button.dart
+++ lib/styled_button.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 class StyleButton extends StatelessWidget {
-  const StyleButton({Key? key}) : super(key: key);
+  const StyleButton({Key? key, required this.icon, required this.onPressed})
+      : super(key: key);
 
   final IconData icon;
   final GestureTapCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return RawMaterialButton(
+      fillColor: Colors.black,
+      splashColor: Colors.redAccent,
+    );
   }
 }
