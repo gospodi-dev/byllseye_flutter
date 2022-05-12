8b6cc869aa58fc594a34f8ed12066debb474014b
Gospodi <gospodijazzet@gmail.com>
2022-05-12 16:31:34 +0300

    добавлен виджет без сохранения состояния

--- lib/styled_button.dart
+++ lib/styled_button.dart
@@ -1,1 +1,8 @@
 import 'package:flutter/material.dart';
+
+class StyleButton extends StatelessWidget {
+  const StyleButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
