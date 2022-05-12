05067b44d56f2851b88757d944dafae13d269e12
Gospodi <gospodijazzet@gmail.com>
2022-05-12 16:53:13 +0300

    добавили обратный вызов

--- lib/styled_button.dart
+++ lib/styled_button.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class StyleButton extends StatelessWidget {
   const StyleButton({Key? key}) : super(key: key);
 
+  final IconData icon;
+  final GestureTapCallback onPressed;
+
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
