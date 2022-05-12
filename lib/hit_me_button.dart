ce5a7a233c7cfaa356d61851b83676584dde19f7
Gospodi <gospodijazzet@gmail.com>
2022-05-12 17:44:51 +0300

    добавлены свойства Строка для текста кнопки и выз.

--- lib/hit_me_button.dart
+++ lib/hit_me_button.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class HitMeButton extends StatelessWidget {
-  const HitMeButton({Key? key}) : super(key: key);
+  const HitMeButton({Key? key, required this.text, required this.onPressed})
+      : super(key: key);
+
+  final String text;
+  final GestureTapCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
