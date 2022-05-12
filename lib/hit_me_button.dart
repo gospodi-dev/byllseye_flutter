7d60d3cc068e0a8f19eeb8c977d8ee2249b7b5a7
Gospodi <gospodijazzet@gmail.com>
2022-05-12 17:42:30 +0300

    добавлен класс для кнопки

--- lib/hit_me_button.dart
+++ lib/hit_me_button.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class HitMeButton extends StatelessWidget {
+  const HitMeButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
