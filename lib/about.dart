f4b1ab596c78efefc492d5d0fc4dbde909fe5810
Gospodi <gospodijazzet@gmail.com>
2022-05-14 11:30:51 +0300

    добавлен appbar с заголовком и оформлением

--- lib/about.dart
+++ lib/about.dart
@@ -4,5 +4,12 @@ class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('О игре "Точно в цель"'),
+        backgroundColor: Colors.red[700],
+      ),
+    );
+  }
 }
