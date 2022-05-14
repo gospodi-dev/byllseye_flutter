1202536b1d988365e54744f68375f64c7ae749ce
Gospodi <gospodijazzet@gmail.com>
2022-05-14 12:30:23 +0300

    добавлены отступы

--- lib/about.dart
+++ lib/about.dart
@@ -25,25 +25,31 @@ class AboutPage extends StatelessWidget {
                 ),
               ),
             ),
-            const Text(
-              'Это Точно в Цель, игра, в которой вы можете выиграть очки и обрести славу перемещая ползунок удачи.\n',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 14.0,
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Это Точно в Цель, игра, в которой вы можете выиграть очки и обрести славу перемещая ползунок удачи.\n',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
               ),
             ),
-            const Text(
-              'Ваша цель - поместить ползунок как можно ближе к целевому значению. Чем ближе вы находитесь, тем больше очков набираете.\n',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 14.0,
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Ваша цель - поместить ползунок как можно ближе к целевому значению. Чем ближе вы находитесь, тем больше очков набираете.\n',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
               ),
             ),
             const Text(
               'Удачи!',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
-                fontSize: 14.0,
+                fontSize: 18.0,
               ),
             ),
             Padding(
