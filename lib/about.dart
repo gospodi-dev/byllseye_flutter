11b876907f91e4342bebf6a9e9db00e07d635dbe
Gospodi <gospodijazzet@gmail.com>
2022-05-14 11:27:05 +0300

    добавлен класс для отображения информации

--- lib/about.dart
+++ lib/about.dart
@@ -1,1 +1,8 @@
 import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
